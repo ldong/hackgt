39eccc91910da4c0f99eaae9067ad87859b6079d
Lin Dong <ldong@gatech.edu>
2014-09-19 22:38:24 -0400

    init

---
+++ FlyerScanner/FlyerScanner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlyerScanner
+//
+//  Created by Lin Dong on 9/19/14.
+//  Copyright (c) 2014 Lin Dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlyerScanner/FlyerScanner/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlyerScanner
+//
+//  Created by Lin Dong on 9/19/14.
+//  Copyright (c) 2014 Lin Dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

b08f3c99ab11d8a5cac0772e6fcdaca9d19e7e3d
Lin Dong <ldong@gatech.edu>
2014-09-20 00:25:44 -0400

    Update FlySwatter to iOS

---
+++ FlySwatter/FlySwatter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FlySwatter
+//
+//  Created by Lin Dong on 9/20/14.
+//  Copyright (c) 2014 Lin Dong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
